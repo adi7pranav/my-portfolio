bb4874fc7bfca3e967d842b31e9654098f41890d
adi7pranav <pranavaditya7@gmail.com>
2025-02-24 22:58:27 +0530

    Changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,14 @@
   });
 
   export const metadata: Metadata = {
-    title: "SDE",
+    title: "Pranav Aditya",
     description: "Portfolio",
   };
 
   export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
       <html lang="en">
-        <body className="scrollbar-hide">
+        <body>
           <ThemeProvider>
             <Background/>
               <Navbar />
