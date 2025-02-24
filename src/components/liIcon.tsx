134f804ded96d4009b580ab2f4c713b1bc5e4e7a
adi7pranav <pranavaditya7@gmail.com>
2025-02-24 22:57:25 +0530

    Added scroll fix for experience page

--- src/components/liIcon.tsx
+++ src/components/liIcon.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { RefObject } from "react";
-import { motion, useScroll, useTransform } from "motion/react";
+import React, { useEffect, useState, RefObject } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-// Define Props for LiIcon
 interface LiIconProps {
   reference: RefObject<HTMLLIElement | null>;
 }
@@ -15,16 +14,22 @@ const LiIcon: React.FC<LiIconProps> = ({ reference }) => {
 
   const scaledProgress = useTransform(scrollYProgress, [0, 1], [0, 1]);
 
+  // Force re-render after mount
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <div>
       {/* Vertical Line */}
       <motion.div
-        style={{ scaleY: scaledProgress }}
-        className="absolute left-[25px]  md:left-[75px] lg:left-[95px] top-0 w-[4px] h-full bg-blue-950 origin-top dark:bg-white"
+        style={{ scaleY: mounted ? scaledProgress : 0 }}
+        className="absolute left-[25px] md:left-[75px] lg:left-[95px] top-0 w-[4px] h-full bg-blue-950 origin-top dark:bg-white"
       />
 
       {/* Icon */}
-      <figure className="absolute left-[-10px] md:left-[40] lg:left-[60px] stroke-dark z-10">
+      <figure className="absolute left-[-10px] md:left-[40px] lg:left-[60px] stroke-dark z-10">
         <svg width="75" height="75" viewBox="0 0 100 100">
           <circle cx="50" cy="50" r="20" className="stroke-blue-950 stroke-1 fill-none" />
           <motion.circle
@@ -32,7 +37,7 @@ const LiIcon: React.FC<LiIconProps> = ({ reference }) => {
             cy="50"
             r="20"
             className="stroke-[5px] fill-white"
-            style={{ pathLength: scrollYProgress }}
+            style={{ pathLength: mounted ? scrollYProgress : 0 }}
           />
           <circle cx="50" cy="50" r="10" className="stroke-1 animate-pulse fill-blue-950" />
         </svg>
